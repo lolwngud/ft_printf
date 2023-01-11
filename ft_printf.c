2474371b7648774331a5c68020dc1d652eaf1dc5
lolwngud <118458655+lolwngud@users.noreply.github.com>
2023-01-11 18:13:14 +0900

    chaos

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: juhypark <juhypark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 16:23:31 by juhypark          #+#    #+#             */
-/*   Updated: 2023/01/04 16:32:37 by juhypark         ###   ########.fr       */
+/*   Updated: 2023/01/11 18:12:38 by juhypark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,21 @@ int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
 	int		print;
+
+	print = 0;
+	va_start(ap, format);
+	while (*format != 0)
+	{
+		if (*format == '%')
+		{
+			format++;
+		}
+		else
+		{
+			write(1, format++, 1);
+			print++;
+		}
+	}
+	va_end(ap);
+	return (print);
 }
